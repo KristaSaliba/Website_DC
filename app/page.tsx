5dd3ba15659a10d0ce3541e36c68357009cfda0d
KristaSaliba <krista@dariuscubed.com>
2026-01-14 10:22:49 +0100

    Remove stats section from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,15 @@
-import Hero from "@/components/hero"
-import Featured from "@/components/featured"
-import Promo from "@/components/promo"
-import Footer from "@/components/footer"
-import Stats from "@/components/stats"
-
-export default function Home() {
-  return (
-    <main>
-      <Hero />
-      <Featured />
-      <Stats />
-      <Promo />
-      <Footer />
-    </main>
-  )
+import Hero from "@/components/hero" 
+import Featured from "@/components/featured" 
+import Promo from "@/components/promo" 
+import Footer from "@/components/footer" 
+ 
+export default function Home() { 
+  return ( 
+    <main> 
+      <Hero /> 
+      <Featured /> 
+      <Promo /> 
+      <Footer /> 
+    </main> 
+  ) 
 }
